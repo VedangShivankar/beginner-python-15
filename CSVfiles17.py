75fb397af2c7abdd53e44577a9927c50d4c255cd
Vedang Shivankar <142177991+VedangShivankar@users.noreply.github.com>
2024-06-18 15:58:24 -0400

    Add files via upload

---
+++ CSVfiles17.py
@@ -0,0 +1,46 @@
+import csv 
+
+#make sure to comment the sections you dont want to use
+
+#section 1
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        csv_writer =  csv.writer(new_file, delimiter='-')
+    
+        for line in csv_reader:
+            csv_writer.writerow(line)
+
+#section 2
+
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'last_name', 'email']
+
+        csv_writer =  csv.DictWriter(new_file,fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+    
+        for line in csv_reader:
+            csv_writer.writerow(line)
+
+#section 3
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_names.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'last_name']
+
+        csv_writer =  csv.DictWriter(new_file,fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+    
+        for line in csv_reader:
+            del line['email']
+            csv_writer.writerow(line)
